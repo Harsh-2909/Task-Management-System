1ee3d731c3717cacf4e27ce5329d7201757a63d4
Harsh Agarwal <harsh29092000@gmail.com>
2021-12-12 00:34:43 +0530

    Added Task list with links to task details

--- task/urls.py
+++ task/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path
 from . import views
-from .views import ProjectListView, ProjectDetailView, ProjectCreateView
-from .views import TaskCreateView, TaskDetailView, TaskDeleteView, TaskUpdateView
+from .views import ProjectListView, ProjectCreateView
+from .views import TaskCreateView, TaskDetailView, TaskDeleteView, TaskUpdateView, TaskListView
 
 urlpatterns = [
     path('', views.home, name= 'home-page'),
+    path('task/list', TaskListView.as_view(), name= 'task-list'),
     path('task/new', TaskCreateView.as_view(), name= 'task-create'),
     path('task/<int:pk>', TaskDetailView.as_view(), name= 'task-detail'),
     path('task/<int:pk>/delete', TaskDeleteView.as_view(), name= 'task-delete'),
     path('task/<int:pk>/update', TaskUpdateView.as_view(), name= 'task-update'),
     path('project/list', ProjectListView.as_view(), name= 'project-list'),
     path('project/new', ProjectCreateView.as_view(), name= 'project-create'),
-    # path('project/<>', ProjectDetailView.as_view(), name= 'project-detail'),
 ]
--- task/views.py
+++ task/views.py
@@ -1,6 +1,7 @@
 from django.http import request
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.models import User
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import (ListView,
 DetailView,
@@ -15,6 +16,17 @@ from task.models import Project, Task
 # Create your views here.
 def home(request):
     return render(request, 'task/home.html', {'title': 'Home'})
+
+class TaskListView(ListView, LoginRequiredMixin):
+    model = Task
+    template_name = 'task/task_list.html'
+    order = ['-created_date', 'name']
+    context_object_name = 'tasks'
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username= self.request.user.username)
+        return Task.objects.filter(assigned_to= user).order_by('-created_date')
+
 class TaskCreateView(CreateView, LoginRequiredMixin):
     model = Task
     fields = ['title', 'description', 'task_list', 'assigned_to']
@@ -25,21 +37,6 @@ class TaskCreateView(CreateView, LoginRequiredMixin):
 
 class TaskDetailView(DetailView, LoginRequiredMixin):
     model = Task
-
-class ProjectCreateView(CreateView, LoginRequiredMixin):
-    model = Project
-    fields = ['name']
-
-class ProjectDetailView(ListView):
-    pass
-#     model = Task
-#     template_name = 'blog/project_detail.html' # <app>/<model>_<viewtype>.html
-#     context_object_name = 'tasks'
-#     paginate_by = 5
-
-#     def get_queryset(self):
-#         # user = get_object_or_404(Task, task_list= self.kwargs.get('username'))
-#         return Task.objects.filter(task_list= self.project.name).order_by('-created_date')
 
 class TaskUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Task
@@ -66,3 +63,7 @@ class ProjectListView(ListView):
     template_name = 'task/project_list.html'
     context_object_name = 'projects'
     ordering = ['name']
+
+class ProjectCreateView(CreateView, LoginRequiredMixin):
+    model = Project
+    fields = ['name']
