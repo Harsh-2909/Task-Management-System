0c171038a02d789b48b68ffd62302fba5fe47fea
Harsh Agarwal <harsh29092000@gmail.com>
2021-12-11 15:10:39 +0530

    Created a boilerplate Home Page

--- task/views.py
+++ task/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
 
 # Create your views here.
+def home(request):
+    return render(request, 'task/home.html', {'title': 'Home'})
--- task_manager/urls.py
+++ task_manager/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('user.urls')),
+    path('user/', include('user.urls')),
+    path('', include('task.urls')),
 ]
