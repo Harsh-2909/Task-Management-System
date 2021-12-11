43b3d79770837ceea3ea5dcf5b838f12917dc5b2
Harsh Agarwal <harsh29092000@gmail.com>
2021-12-11 15:36:34 +0530

    added profile pic to user profile

--- user/forms.py
+++ user/forms.py
@@ -1,5 +1,4 @@
 from django import forms
-from django import forms
 from .models import Profile
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
@@ -11,3 +10,18 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+class UserUpdateForm(forms.ModelForm):
+    '''The form that would be used to update a user'''
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+class ProfileUpdateForm(forms.ModelForm):
+    '''The form that would be used to update a profile'''
+
+    class Meta:
+        model = Profile
+        fields = ['image']
