835cf520a54c0158998b377ee710218ca33c6681
Harsh Agarwal <harsh29092000@gmail.com>
2021-12-11 15:58:15 +0530

    Added Password reset mechanism

--- user/urls.py
+++ user/urls.py
@@ -7,6 +7,16 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name= 'user/login.html'), name= 'user-login'),
     path('logout/', auth_views.LogoutView.as_view(template_name= 'user/logout.html'), name= 'user-logout'),
     path('profile/', views.profile, name= 'user-profile'),
-
-        
+    path('password-reset/',
+    auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'),
+    name= 'password_reset'),
+    path('password-reset/done/',
+    auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),
+    name= 'password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/',
+    auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),
+    name= 'password_reset_confirm'),
+    path('password-reset-complete/',
+    auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),
+    name= 'password_reset_complete'),
 ]
