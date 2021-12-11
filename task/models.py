c7f7aa8a4c7cfdc92b95e25a58589a4113472fb0
Harsh Agarwal <harsh29092000@gmail.com>
2021-12-11 14:06:47 +0530

    Created the Task datamodel

--- task/models.py
+++ task/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
+class Task(models.Model):
+    '''The Task dataclass to store the task in database'''
+    title = models.CharField(max_length= 50)
+    description = models.TextField(max_length= 500)
+    assigned = models.ForeignKey(User, on_delete= models.CASCADE)
+    # attachments = models.FileField()
+
+    def __str__(self):
+        return self.title
